7ae542df3447219c7c7811b917335496e5e11f4b
Lavallee,Dr.,Martin (MED MA) BIP-US-R <martin.lavallee@boehringer-ingelheim.com>
2025-02-24 12:00:12 -0500

    fix age bug and cts null in sd

--- inst/sql/demographics/demoAge.sql
+++ inst/sql/demographics/demoAge.sql
@@ -13,7 +13,14 @@ FROM (
       t.cohort_definition_id,
       t.subject_id,
       YEAR(t.cohort_start_date) - d.year_of_birth AS value
-    FROM @target_table t
+    FROM (
+      SELECT a.cohort_definition_id, a.subject_id, a.cohort_start_date, a.cohort_end_date
+        FROM(
+          SELECT *, ROW_NUMBER() OVER (PARTITION BY cohort_definition_id, subject_id ORDER BY cohort_start_date) AS rn
+          FROM @target_table
+          ) a
+        WHERE a.rn = 1
+    ) t
     JOIN @cdm_database_schema.person d
     ON t.subject_id = d.person_id
 ) a
