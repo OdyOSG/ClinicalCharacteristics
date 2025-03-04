584a8b70d076e9e078fef95a71e484401640784c
Lavallee,Dr.,Martin (MED MA) BIP-US-R <martin.lavallee@boehringer-ingelheim.com>
2025-03-04 15:43:18 -0500

    fix bug in cohort count char

--- inst/sql/cohort/observedCount.sql
+++ inst/sql/cohort/observedCount.sql
@@ -7,7 +7,7 @@ SELECT
         d.ordinal_id,
         d.statistic_type,
         d.line_item_class,
-        COUNT(DISTINCT d.event_date) AS value
+        COUNT(DISTINCT d.event_start_date) AS value
 FROM (
   SELECT d.*, m.ordinal_id, m.statistic_type, m.line_item_class
   FROM @cohort_occurrence_table d
@@ -15,7 +15,7 @@ FROM (
     SELECT * FROM @ts_meta_table WHERE statistic_type = 'CategoricalPrevalence'
   ) m
   ON d.raw_occurrence_id = m.value_id AND d.time_label = m.time_label
-  WHERE d.cohort_end_date >= d.event_date
+  WHERE d.cohort_end_date >= d.event_start_date
 ) d
 GROUP BY d.target_cohort_id, d.subject_id, d.time_label, d.domain_table, d.raw_occurrence_id
 ;
