a98f551f5bd422c86f0ba8f4e85a46474f7d5efd
Lavallee,Dr.,Martin (MED MA) BIP-US-R <martin.lavallee@boehringer-ingelheim.com>
2025-06-10 15:58:15 -0400

    fix demo payer

--- inst/sql/demographics/demoPayerType.sql
+++ inst/sql/demographics/demoPayerType.sql
@@ -1,27 +1,32 @@
 /* Identify Patient payer type (rank by lowest payer concept)*/
 INSERT INTO @patient_level_data
 SELECT
-  b.cohort_definition_id AS target_cohort_id,
-  b.subject_id,
+  c.cohort_definition_id AS target_cohort_id,
+  c.subject_id,
   'Static at Index' AS time_label,
   'payer_plan_period' AS domain_table,
   'payer_type' AS patient_line,
   'payer_concept_id' AS value_type,
   -999 AS value_id,
-  b.payer_concept_id AS value
+  c.payer_concept_id AS value
 FROM (
-  SELECT a.cohort_definition_id, a.subject_id, a.payer_concept_id
-    FROM(
+  SELECT b.cohort_definition_id, b.subject_id, b.payer_concept_id
+  FROM(
+    SELECT *,
+        ROW_NUMBER() OVER (PARTITION BY cohort_definition_id, subject_id ORDER BY t_diff, payer_concept_id DESC) AS rn
+    FROM (
       SELECT
-        t.*,
-        d.payer_concept_id,
-        ROW_NUMBER() OVER (PARTITION BY t.cohort_definition_id, t.subject_id ORDER BY d.payer_plan_period_id, d.payer_concept_id DESC) AS rn
+            t.*,
+            d.payer_concept_id,
+            d.payer_plan_period_start_date,
+            d.payer_plan_period_end_date,
+            ABS(DATEDIFF(day, t.cohort_start_date, d.payer_plan_period_start_date)) as t_diff
       FROM @target_table t
       JOIN @cdm_database_schema.payer_plan_period d
         ON t.subject_id = d.person_id
-        AND t.cohort_start_date BETWEEN d.payer_plan_period_start_date AND d.payer_plan_period_end_date
+        AND (t.cohort_end_date <= d.payer_plan_period_end_date OR t.cohort_start_date <= d.payer_plan_period_end_date)
       ) a
-    WHERE a.rn = 1
-  ) b
-
+    ) b
+    WHERE b.rn = 1
+) c
 ;
